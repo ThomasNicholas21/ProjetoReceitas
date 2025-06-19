e118874be8074854f4818c9743dcc3f07b065c29
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-19 16:46:47 -0300

    fix: ajuste de template

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(
         request=request,
-        template_name='pages/home.html',
+        template_name='recipes/pages/home.html',
     )
