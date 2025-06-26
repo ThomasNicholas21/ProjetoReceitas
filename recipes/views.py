8760e9e8b8c9d4eba16796142700b39a8c1c1c67
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-25 21:55:47 -0300

    feat: removendo html desnecessário para page detail

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.data_factory import make_recipe
 
 
 def home(request):
     return render(
         request=request,
         template_name='recipes/pages/home.html',
+        context={
+            'recipes': [make_recipe() for _ in range(10)],
+        }
     )
 
 
@@ -12,4 +16,8 @@ def recipe(request, id_recipe):
     return render(
         request=request,
         template_name='recipes/pages/recipe.html',
+        context={
+            'recipe': make_recipe(),
+            'is_detail_page': True
+        }
     )
