75179e63a83b6b7f5a8d4e84ad9bb503bc9ce30a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-01 21:06:39 -0300

    feat: implementando classes no painél admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes.models import Recipe, Category
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
