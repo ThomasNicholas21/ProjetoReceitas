9bbb2d1dfd5e6e514db85cb8525b51eb42861ad0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-03 18:42:43 -0300

    feat: incluindo view category e ajustando filtros

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all()
+    recipes = (
+        Recipe
+        .objects
+        .filter(
+            is_published=True
+            )
+        ).order_by(
+            '-id'
+        )
     context = {
         'recipes': recipes,
     }
@@ -16,10 +24,34 @@ def home(request):
     )
 
 
+def category(request, id_category):
+    recipe = (
+        Recipe
+        .objects
+        .order_by(
+            '-id'
+        )
+    )
+    recipes = get_list_or_404(
+        recipe,
+        category__id=id_category,
+        is_published=True
+    )
+    context = {
+            'recipes': recipes,
+        }
+    return render(
+        request=request,
+        template_name='recipes/pages/category.html',
+        context=context
+    )
+
+
 def recipe(request, id_recipe):
     recipe = get_object_or_404(
         Recipe,
-        id=id_recipe
+        id=id_recipe,
+        is_published=True
     )
     context = {
             'recipe': recipe,
