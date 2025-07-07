0381772f9eedbd7b5b852dfe054c04eb7421e618
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-07 13:50:21 -0300

    tests: testando URLS

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_recipes_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+
+    def test_recipes_detail_url_is_correct(self):
+        recipe_url = reverse(
+            'recipes:recipe',
+            kwargs={
+                'id_recipe': 1
+            }
+            )
+        self.assertEqual(recipe_url, '/recipes/1/')
+
+    def test_recipes_category_url_is_correct(self):
+        category_url = reverse(
+            'recipes:category',
+            kwargs={
+                'id_category': 1
+                }
+            )
+        self.assertEqual(category_url, '/recipes/category/1/')
