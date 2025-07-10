6a333f9cc1ca8a8be91f284f4a1548fa80d4a653
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-10 13:27:52 -0300

    tests: testando valores padrões

--- recipes/tests/test_recipe_model.py
+++ recipes/tests/test_recipe_model.py
@@ -1,12 +1,30 @@
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 from recipes.tests.test_recipes_fixture import RecipeFixture
+from recipes.models import Recipe
 
 
 class RecipeModelTest(RecipeFixture):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            title='Title Test Html',
+            description='Description Test Html',
+            slug='test-slug-html',
+            preparation_time=10,
+            preparation_time_unit='minutos',
+            servings=5,
+            servings_unit='Serving unit Html',
+            preparation_steps='Preparation steps Html',
+            author=self.make_author(username='Test Auhtor'),
+            category=self.make_category(name='Test'),
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
 
     @parameterized.expand(
             [
@@ -16,8 +34,25 @@ class RecipeModelTest(RecipeFixture):
                 ('servings_unit', 64),
             ]
         )
-    def test_recipe_model_fields_max_length(self, field, max_length):
-        """"Test fields that have max_length"""
+    def test_recipe_model_fields_with_max_length(self, field, max_length):
+        """"Test fields that have max_length param"""
         setattr(self.recipe, field, 'Teste' * max_length)
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    @parameterized.expand(
+            [
+                ('preparation_steps_is_html', False),
+                ('is_published', False),
+                ('cover', 'default/hamburguer.png'),
+            ]
+    )
+    def test_recipe_model_fields_with_default(self, field, default):
+        """Test fields that have default param"""
+        recipe = self.make_recipe_no_defaults()
+        current_atribute_value = getattr(recipe, field)
+        self.assertEqual(
+            current_atribute_value,
+            default,
+            msg=f'"{field}" default value is not "{current_atribute_value}"'
+        )
