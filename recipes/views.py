3cfc8b012d2ed7a6fa13120df6520b6a415fbd1d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-12 16:45:47 -0300

    feat: incluindo filtros

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.http import Http404
+from django.db.models import Q
 from recipes.models import Recipe
 
 
@@ -76,6 +77,16 @@ def search(request: HttpRequest) -> HttpResponse:
 
     if not search_request:
         raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_request) |
+            Q(description__icontains=search_request)
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    context['recipes'] = recipes
 
     return render(
         request=request,
