9409ba1cbb09e6a556dc723718ac8200eb9cc459
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-14 17:42:52 -0300

    feat: implementando função para paginação

--- utils/recipes/pagination.py
+++ utils/recipes/pagination.py
@@ -1,6 +1,34 @@
+from math import ceil
+
+
 def make_pagination_range(
         page_range,
         pages,
         current_page,
 ):
-    return [1, 2, 3, 4]
+    middle_range = ceil(pages / 2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+    total_pages = len(page_range)
+
+    offset_range = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += offset_range
+
+    if stop_range >= total_pages:
+        start_range = start_range - abs(total_pages - stop_range)
+
+    pagination = page_range[start_range:stop_range]
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'pages': pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages
+    }
