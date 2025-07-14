548b5c76a94e02dafd14c7fd5d73dbaad7929cb5
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-14 17:42:39 -0300

    tests: testando paginação

--- utils/test/test_pagination.py
+++ utils/test/test_pagination.py
@@ -8,5 +8,71 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             pages=4,
             current_page=1
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_range_is_static_if_current_page_is_less_than_middle_page(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=1
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=2
+        )['pagination']
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=3
+        )['pagination']
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+    def test_make_pagination_range_middle_ranges_are_correct(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=10
+        )['pagination']
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=12
+        )['pagination']
+        self.assertEqual([11, 12, 13, 14], pagination)
+
+    def test_make_pagination_static_if_last_page_is_next(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=18
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=19
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=20
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            pages=4,
+            current_page=21
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
