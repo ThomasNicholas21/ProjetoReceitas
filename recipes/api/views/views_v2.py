93fc37228f59a477f7e71daf5b9752316eb862d1
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-09-02 19:53:47 -0300

    feat: incluindo v2 das API usando APIView

---
+++ recipes/api/views/views_v2.py
@@ -0,0 +1,45 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from recipes.models import Recipe
+from recipes.api.serializer import RecipeSerializer
+
+
+class RecipesApiView(APIView):
+    def get(self, request):
+        recipes = Recipe.objects.all().order_by("id")
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class DetailRecipesApiView(APIView):
+    def get(self, request, pk):
+        recipe = Recipe.objects.get(pk=pk)
+        serializer = RecipeSerializer(recipe)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        serializer = RecipeSerializer(
+            data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        recipe = Recipe.objects.get(pk=pk)
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
