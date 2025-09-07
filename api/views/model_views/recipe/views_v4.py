85911564752bab61b57a8f54512555b739f88dbb
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-09-07 18:56:47 -0300

    feat: implementando permissão personalizada

--- api/views/model_views/recipe/views_v4.py
+++ api/views/model_views/recipe/views_v4.py
@@ -1,18 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from recipes.models import Recipe
 from api.views.model_views.serializer import RecipeSerializer
 from api.paginator import DefaultPaginationOffset
+from api.permissions import IsOwner
 
 
 class ViewSetRecipes(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     pagination_class = DefaultPaginationOffset
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "PUT", "DELETE"]:
+            return [IsOwner(),]
+
+        return super().get_permissions()
 
     def partial_update(self, request: Request, *args, **kwargs):
         pk = kwargs.get('pk')
