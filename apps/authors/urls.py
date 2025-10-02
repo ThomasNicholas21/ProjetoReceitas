342d640b8e3c25bd25dc30689fbd3df36ed1500c
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-10-01 22:17:34 -0300

    feat: implementing url

--- apps/authors/urls.py
+++ apps/authors/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from apps.authors import views
 
 
+app_name = "authors"
+
+
 urlpatterns = [
-    path("register/", views.register_view, name="register")
+    path("register/", views.register_view, name="register"),
+    path("register/create", views.register_create, name="create"),
 ]
