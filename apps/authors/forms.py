c7264266a862cb635f8319889bd66b01fee8bd60
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-10-18 13:25:51 -0300

    feat: implementing forms fields

--- apps/authors/forms.py
+++ apps/authors/forms.py
@@ -3,6 +3,59 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label="Name",
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Name",
+            }
+        )
+    )
+    last_name = forms.CharField(
+        label="Last name",
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Last name",
+            }
+        )
+    )
+    username = forms.CharField(
+        label="Username",
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Username",
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        max_length=128,
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+            }
+        )
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+            }
+        )
+    )
+    confirm_password = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Confirm password",
+            }
+        )
+    )
+
     class Meta:
         model = User
         fields = [
