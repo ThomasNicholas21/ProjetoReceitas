72deaaffc0998a6d7781740e208980ac5582e287
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-11-15 20:20:44 -0300

    feat: implementing validator for a strong password

--- apps/authors/forms.py
+++ apps/authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -10,6 +12,15 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, "placeholder", placeholder_val)
+
+
+def validate_password(password):
+    regex = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$")
+    if not regex.match(password):
+        raise ValidationError(
+            "Not a strong password!",
+            code="invalid"
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -33,7 +44,8 @@ class RegisterForm(forms.ModelForm):
             "Password must have at least one uppercase letter, "
             "one lowercase letter and one number. The length should be "
             "at least 8 characters."
-        )
+        ),
+        validators=[validate_password,]
     )
     confirm_password = forms.CharField(
         required=True,
